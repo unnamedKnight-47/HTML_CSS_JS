88deeffaa5f5ad38e8b6b25cf007b7f5bfd35dd1
unnamedKnight <atikeprofessional117@gmail.com>
2025-09-27 14:50:40 +0600

    Add close navigation functionality

---
+++ CSS/Responsive_Web_Design/The_final_project/script.js
@@ -0,0 +1,9 @@
+// console.log("Hello world");
+
+const closeButton = document.querySelector(".close-nav");
+const nav = document.querySelector(".nav");
+
+
+closeButton.addEventListener("click", ()=> {
+    nav.classList.remove("navigation-open");
+});
