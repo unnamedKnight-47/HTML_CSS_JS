44effc999c68b2d2674db3c046e95e08367579d8
unnamedKnight <atikeprofessional117@gmail.com>
2025-09-27 15:51:00 +0600

    Done with mobile navigation

--- CSS/Responsive_Web_Design/The_final_project/script.js
+++ CSS/Responsive_Web_Design/The_final_project/script.js
@@ -1,9 +1,14 @@
 // console.log("Hello world");
 
 const closeButton = document.querySelector(".close-nav");
+const openButton = document.querySelector(".open-nav");
 const nav = document.querySelector(".nav");
 
 
 closeButton.addEventListener("click", ()=> {
     nav.classList.remove("navigation-open");
+});
+
+openButton.addEventListener("click", ()=>{
+    nav.classList.add("navigation-open");
 });
